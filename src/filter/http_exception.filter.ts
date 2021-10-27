aadf0c99cc3f9a35b90c0524cb060a98b7465071
러리 <doralife12@naver.com>
2021-10-28 01:45:20 +0900

    Feat: Http exception filter

---
+++ src/filter/http_exception.filter.ts
@@ -0,0 +1,18 @@
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+} from '@nestjs/common';
+import { Response } from 'express';
+import { ErrorType, Resp } from 'src/common/response';
+
+@Catch(HttpException)
+export class HttpExceptionFilter implements ExceptionFilter {
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const data: ErrorType = Resp.error(exception.getStatus());
+    response.status(data.status).json(data);
+  }
+}
