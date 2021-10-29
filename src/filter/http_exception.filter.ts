120672cf978d05a914d24191f5efca5f7728b12f
러리 <doralife12@naver.com>
2021-10-30 00:44:28 +0900

    Refactor: Unnecessary error handling code

--- src/filter/http_exception.filter.ts
+++ src/filter/http_exception.filter.ts
@@ -5,14 +5,12 @@ import {
   HttpException,
 } from '@nestjs/common';
 import { Response } from 'express';
-import { ErrorType, Resp } from 'src/common/response';
 
 @Catch(HttpException)
 export class HttpExceptionFilter implements ExceptionFilter {
   catch(exception: HttpException, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
-    const data: ErrorType = Resp.error(exception.getStatus());
-    response.status(data.code).json(data);
+    response.status(exception.getStatus()).json(exception.getResponse());
   }
 }
