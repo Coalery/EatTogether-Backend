02467114fd11ff101c073b6d92c6395235af76d5
러리 <doralife12@naver.com>
2021-11-04 22:41:32 +0900

    Feat: Participate to party

---
+++ src/participate/participate.service.ts
@@ -0,0 +1,38 @@
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Party } from 'src/party/party.entity';
+import { User } from 'src/user/user.entity';
+import { Repository, UpdateResult } from 'typeorm';
+
+@Injectable()
+export class ParticipateService {
+  constructor(
+    @InjectRepository(Party) private partyRepository: Repository<Party>,
+  ) {}
+
+  async participateToParty(
+    partyId: number,
+    requestor: User,
+  ): Promise<UpdateResult> {
+    const party: Party = await this.partyRepository.findOne(partyId);
+    if (!party) {
+      throw new HttpException(
+        "Can't find party with given id.",
+        HttpStatus.NOT_FOUND,
+      );
+    }
+
+    const isParticipated =
+      party.participant.filter((user) => user.id === requestor.id).length !== 0;
+    if (isParticipated) {
+      throw new HttpException(
+        `User ${requestor.name} participated ${party.title} already.`,
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    return await this.partyRepository.update(partyId, {
+      participant: [...party.participant, requestor],
+    });
+  }
+}
